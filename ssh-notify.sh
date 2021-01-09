87df09aa5894e7488aa52f527a1209dbf8331f2c
Samuel <samuel-barret@sfr.fr>
2021-01-09 21:39:55 +0100

    Modification du script de connexion

--- ssh-notify.sh
+++ ssh-notify.sh
@@ -1,43 +1,82 @@
-#!/bin/sh
-#
-# title             : ssh_notify
-# description       : Notification des connexions ssh sur Discord.
-# author            : TutoRapide
-# date              : 2021-06-01
-# version           : 0.1.0
-# usage             : Dans dans le fichier dans /etc/profile.d/ssh-notify.sh
-#===============================================================================
+# Config {
 
 BOTNAME=SSH-Notify #Nom du webhook
+AVATAR_URL="https://icons.iconarchive.com/icons/blackvariant/button-ui-system-apps/512/Terminal-icon.png"
+WEBHOOK="https://discord.com/api/webhooks/796733904777379840/3PcW_K5riB9IhM7v2onZ5ibOnmTSiWE-jhwiMADiNu2mcpc8RxLe50DrQS0S9DoB_69R"
 DATE=$(date +"%m-%d-%Y-%H:%M:%S") #Date + heure
-
-USER_IP=`echo $SSH_CLIENT | awk '{ print $1}'` #Ipv4 de l'utilisateur
-ipP= dig +short myip.opendns.com @resolver1.opendns.com > /dev/null 2>&1  #Ip de la machine et cache l'ip au lancement du script
 
 TMPFILE=$(mktemp) #Creation d'un fichier temporaire dans /tmp
 
+IGNORED_USERS="git" #Définir la liste des utilisateurs à ignorer, espacer les utilisateur ou , pour séparer les utilisateurs
 
-curl -s "http://ip-api.com/json/${USER_IP}" > $TMPFILE 
- 
+#~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ }
+
+# Macros {
+
+	# String in array  ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ |
+	function inArray() {
+		[[ `echo "${2}" | grep "\b${1}\b"` ]] && return 0
+	}
+
+	# Is a valid number  ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ |
+	function isNumber() {
+		[[ "$1" -eq "$1" ]] && return 0
+	}
+
+	# Chech if the input is a even number  ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ |
+	function isEven() {
+		return [ $((HISTORY_COUNTER%2)) -eq 0 ]
+	}
+
+	# Is IP valid  ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ |
+	function isValidIp() {
+		local ip=$1
+		local stat=1
+
+		if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+			OIFS=$IFS
+			IFS='.'
+			ip=($ip)
+			IFS=$OIFS
+			[[ ${ip[0]} -le 255 && ${ip[1]} -le 255 \
+				&& ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
+			stat=$?
+		fi
+		return $stat
+	}
+
+#~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ }
+
+
+
+ME="`whoami`"
+
+IP=`echo $SSH_CLIENT | awk '{ ip = $1 } END { print ip }'`
+PTR=`dig +short -x ${IP} | sed s/\.$//`
+
+curl -s "http://ip-api.com/json/${PTR}" > $TMPFILE 
+
 curl --silent -v \
 -H "Content-Type: application/json" \
 -X POST \
--d '{"username": "'"$BOTNAME"'", "avatar_url": "https://icons.iconarchive.com/icons/blackvariant/button-ui-system-apps/512/Terminal-icon.png", 
+-d '{"username": "'"$BOTNAME"'", "avatar_url": "'"$AVATAR_URL"'", 
     "embeds": [{ 
             "color": 12976176, 
             "title": "SSH-Notification",
             "thumbnail": {
-                "url": "https://icons.iconarchive.com/icons/blackvariant/button-ui-system-apps/512/Terminal-icon.png"
+                "url": "'"$AVATAR_URL"'"
             },
             "author": {
                 "name": "'"$BOTNAME"'",
-                "icon_url": "https://icons.iconarchive.com/icons/blackvariant/button-ui-system-apps/512/Terminal-icon.png"
+                "icon_url": "'"$AVATAR_URL"'"
             },
             "footer": {
-                "icon_url": "https://icons.iconarchive.com/icons/blackvariant/button-ui-system-apps/512/Terminal-icon.png",
+                "icon_url": "'"$AVATAR_URL"'",
                 "text": "'"$BOTNAME"'"
             },
-            "description":  "**Détails**\n \\👤 Utilisateur: '\`$(whoami)\`',\n \\🖥️ Host: '\`$(hostname)\`' \n \\🕐 Connexion: '\`$DATE\`',\n\n **Adresse IP**\n 📡 IP: '\`${USER_IP}\`',\n \\🛰️ Appareil '\`$(dig -x $USER_IP +short)\`',\n \\🌎 Pays: '\`$(cat $TMPFILE | jq -r .country)\`' \n \\🌐 Region: '\`$(cat $TMPFILE | jq -r .regionName)\`',\n \\🔰 Ville: '\`$(cat $TMPFILE | jq -r .city)\`',\n \\📠 ISP: '\`$(cat $TMPFILE | jq -r .isp)\`' "
+            "description":  "**Détails**\n \\👤 Utilisateur: '\`${ME}\`',\n \\🖥️ Host: '\`$(hostname)\`' \n \\🕐 Connexion: '\`$DATE\`' ,\n\n **Adresse IP**\n 📡 IP: '\`${IP}\`',\n \\🌎 Pays: '\`$(cat $TMPFILE | jq -r .country)\`' \n \\🌐 Region: '\`$(cat $TMPFILE | jq -r .regionName)\`',\n \\🔰 Ville: '\`$(cat $TMPFILE | jq -r .city)\`',\n \\📠 ISP: '\`$(cat $TMPFILE | jq -r .isp)\`' "
        }] 
     }' \
-https://discord.com/api/webhooks/796733904777379840/3PcW_K5riB9IhM7v2onZ5ibOnmTSiWE-jhwiMADiNu2mcpc8RxLe50DrQS0S9DoB_69R > /dev/null 2>&1 
+$WEBHOOK > /dev/null 2>&1 
+
+
